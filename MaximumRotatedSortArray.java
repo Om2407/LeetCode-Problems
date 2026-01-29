e876a5b3360801825befbf11766f509fe0030cf0
Om Gupta <your_email@example.com>
2026-01-29 20:07:06 +0530

    medium level problems

--- MaximumRotatedSortArray.java
+++ MaximumRotatedSortArray.java
@@ -1,27 +1,25 @@
 public class MaximumRotatedSortArray {
-    //like minimumrotatedsortarray.java
     public static int findMax(int nums[]){
 
         int left = 0;
-        int right= nums.length-1;
+        int right = nums.length-1;
 
         while(left<right){
             int mid = left + (right-left)/2;
-            if(nums[mid]>nums[mid+1]){
+            if(nums[mid] > nums[mid+1]){
                 return nums[mid];
             }
             if(nums[mid]> nums[left]){
-                left = mid+1;
-            }else{
+                left = mid + 1;
+            }
+            else{
                 right = mid;
             }
         }
         return nums[left];
-
     }
-
     public static void main(String args[]){
-        int nums[] ={4,5,6,7,8,9,0,1,2};
+        int nums[] = {11,15,99,0,1,2,3};
         System.out.println(findMax(nums));
     }
 }
