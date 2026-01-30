b2761a2c7d5e248b9dd024590e63ed974db107a8
Om Gupta <your_email@example.com>
2026-01-30 19:15:32 +0530

    Daily Problems

--- SubArraySumEqK.java
+++ SubArraySumEqK.java
@@ -1,20 +1,20 @@
 import java.util.HashMap;
 
 public class SubArraySumEqK {
-public static void main(String args[]){
-    int nums[] ={1,1,1};
-    int k = 2;
-    HashMap<Integer,Integer> map = new HashMap<>();
-map.put(0,1);
-    int prefixSum = 0;
-    int count = 0;
-    for(int i=0; i<nums.length; i++){
-        prefixSum += nums[i];
-        if(map.containsKey(prefixSum-k)){
-        count += map.get(prefixSum-k);
+    public static void main(String args[]) {
+        int nums[] = { 1, 1, 1 };
+        int k = 2;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int prefixSum = 0;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            prefixSum += nums[i];
+            if (map.containsKey(prefixSum - k)) {
+                count += map.get(prefixSum - k);
+            }
+            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
+        }
+        System.out.println(count);
     }
-    map.put(prefixSum, map.getOrDefault(prefixSum,0) + 1);
-}
-System.out.println(count);
-}
 }
